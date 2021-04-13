3dcc983ab22b83d5f91c1c8faa4ba33be3ab5671
Casper <ckleinessink@gmail.com>
2021-04-13 12:45:09 +0200

    reverted delex.py to working order

--- delex.py
+++ delex.py
@@ -22,15 +22,25 @@ signal.signal (signal.SIGALRM, handler)
 
 # locate trainset and annotations
 trainset = pd.read_csv("e2e-dataset/trainset.csv")
+annotations = trainset.iloc[:,0]
+texts = trainset.iloc[:,1]
+
+# create empty dataframe
 d = {}
 ts = pd.DataFrame(data=trainset)
 
+#types = ["name", "priceRange", "near", "customer rating"]
 types = ["name", "eatType", "familyFriendly", "priceRange", "food", "near", "area", "customer rating"]
 
-for i in range(1,20): #len(ts)
-    signal.alarm(15)
-    try:
-        for x in types:
+print(len(ts))
+
+
+# #for all types
+for x in types:
+    # over the whole dataframe
+    for i in range(3000,3010): #len(ts)
+        signal.alarm(15)
+        try:
             # store the mr value in between brackets (e.g. for type name: name["The Vaults"] => "The Vaults")
             name = re.search(x + '\[([^]]+)\]', ts.iloc[i,0])
             # familyFriendly is boolean, so we should create a matching string
@@ -49,12 +59,12 @@ for i in range(1,20): #len(ts)
                         found_numbers = regex.search(r"(?i)(?:"+ numbers +")", ts.iloc[i, 1])
                         if found_numbers:
                             name = numbers
-                # find matching word, include letters after word (e.g. searching "high" in "highly" should return whole "highly", not just "high")
+
                 found_name = regex.search(r"(?i)(?:"+ name +")", ts.iloc[i, 1])
                 # replace that best match
                 if found_name:
                     found_name = found_name.group(0)
-                    ts.iloc[i, 1] = re.sub(str(found_name), x + "_x", ts.iloc[i, 1], flags=re.IGNORECASE, count=1)
+                    ts.iloc[i,1] = re.sub(str(found_name), x + "_x", ts.iloc[i,1], flags=re.IGNORECASE, count=1)
                 else:
                     # look for the best match in the ref (e.g. for priceRange: £20-25, £20-£25 is also fine)
                     # ?b: best match with error {e<=x}
@@ -64,22 +74,13 @@ for i in range(1,20): #len(ts)
                     found_name = regex.search(r"(?b)(?i)(?:\w+" + name + "\w+){e<=" + str(e) + "}", ts.iloc[i, 1])
                     if found_name:
                         found_name = found_name.group(0)
-                        ts.iloc[i,1] = re.sub(str(found_name), x + "_x", ts.iloc[i,1], flags=re.IGNORECASE, count=1)
-                    else:
-                        # look for the best match in the ref (e.g. for priceRange: £20-25, £20-£25 is also fine)
-                        # ?b: best match with error {e<=x}
-                        # i?: ignore case
-                        # \w+: include whole words (e.g. went wrong with match on "tart at", had to include "start at")
-                        e = len(name) - len(name.split())
-                        found_name = regex.search(r"(?b)(?i)(?:\w+" + name + "\w+){e<=" + str(e) + "}", ts.iloc[i, 1])
-                        if found_name:
-                            found_name = found_name.group(0)
-                            ts.iloc[i, 1] = re.sub(str(found_name), x + "_x", ts.iloc[i, 1], flags=re.IGNORECASE, count=1)
-                    # replace the value in between brackets in the mr
-                    ts.iloc[i,0] = re.sub(str(name), x + "_x", ts.iloc[i,0], flags=re.IGNORECASE, count=1)
-    except Exception:
-        print("took too long", i)
-        ts.iloc[i,1] = "XXX"
-        continue
+                        ts.iloc[i, 1] = re.sub(str(found_name), x + "_x", ts.iloc[i, 1], flags=re.IGNORECASE, count=1)
+                # replace the value in between brackets in the mr
+                ts.iloc[i,0] = re.sub(str(name), x + "_x", ts.iloc[i,0], flags=re.IGNORECASE, count=1)
+        except Exception:
+            print("took too long", i)
+            ts.iloc[i,1] = "XXX"
+            continue
+        # print(ts.iloc[[i]])
 
 ts.to_csv(r'delexicalized/delex4.csv')
