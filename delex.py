6c310f8fa19ba50a69c1f3ef291737c07c1cb916
Casper <ckleinessink@gmail.com>
2021-04-13 11:02:51 +0200

    added timeout and bad row indication

--- delex.py
+++ delex.py
@@ -1,7 +1,17 @@
 import pandas as pd
 import re
 import regex
+import signal
+import time
 
+class TimeoutError (RuntimeError):
+    pass
+
+def handler (signum, frame):
+    raise Exception()
+    # pass
+
+signal.signal (signal.SIGALRM, handler)
 
 # replacing the values of the attributes with placeholders
 
@@ -24,44 +34,53 @@ types = ["name", "eatType", "familyFriendly", "priceRange", "food", "near", "are
 
 print(len(ts))
 
+
 # #for all types
 for x in types:
     # over the whole dataframe
-    for i in range(5): #len(ts)
-        # store the mr value in between brackets (e.g. for type name: name["The Vaults"] => "The Vaults")
-        name = re.search(x + '\[([^]]+)\]', ts.iloc[i,0])
-        # familyFriendly is boolean, so we should create a matching string
-        # customer rating is not always the whole string but just numbers, so first try to find it on numbers
-        if name:
-            name = name.group(1)
-            if x == "familyFriendly":
-                name = "family friendly"
-            elif x == "customer rating":
-                numbers = [int(s) for s in name.split() if s.isdigit()]
-                if numbers:
-                    # remove double numbers
-                    numbers = set(numbers)
-                    numbers = [str(n) for n in numbers]
-                    numbers = " ".join(numbers)
-                    found_numbers = regex.search(r"(?i)(?:"+ numbers +")", ts.iloc[i, 1])
-                    if found_numbers:
-                        name = numbers
+    for i in range(2100,3000): #len(ts)
+        signal.alarm(10)
+        try:
+            # store the mr value in between brackets (e.g. for type name: name["The Vaults"] => "The Vaults")
+            name = re.search(x + '\[([^]]+)\]', ts.iloc[i,0])
+            # familyFriendly is boolean, so we should create a matching string
+            # customer rating is not always the whole string but just numbers, so first try to find it on numbers
+            if name:
+                name = name.group(1)
+                if x == "familyFriendly":
+                    name = "family friendly"
+                elif x == "customer rating":
+                    numbers = [int(s) for s in name.split() if s.isdigit()]
+                    if numbers:
+                        # remove double numbers
+                        numbers = set(numbers)
+                        numbers = [str(n) for n in numbers]
+                        numbers = " ".join(numbers)
+                        found_numbers = regex.search(r"(?i)(?:"+ numbers +")", ts.iloc[i, 1])
+                        if found_numbers:
+                            name = numbers
 
-            found_name = regex.search(r"(?i)(?:"+ name +")", ts.iloc[i, 1])
-            # replace that best match
-            if found_name:
-                found_name = found_name.group(0)
-                ts.iloc[i,1] = re.sub(str(found_name), x + "_x", ts.iloc[i,1], flags=re.IGNORECASE, count=1)
-            else:
-                # look for the best match in the ref (e.g. for priceRange: £20-25, £20-£25 is also fine)
-                # ?b: best match with error {e<=x}
-                # i?: ignore case
-                # \w+: include whole words (e.g. went wrong with match on "tart at", had to include "start at")
-                e = len(name) - len(name.split())
-                found_name = regex.search(r"(?b)(?i)(?:\w+" + name + "\w+){e<=" + str(e) + "}", ts.iloc[i, 1])
+                found_name = regex.search(r"(?i)(?:"+ name +")", ts.iloc[i, 1])
+                # replace that best match
                 if found_name:
                     found_name = found_name.group(0)
-                    ts.iloc[i, 1] = re.sub(str(found_name), x + "_x", ts.iloc[i, 1], flags=re.IGNORECASE, count=1)
-            # replace the value in between brackets in the mr
-            ts.iloc[i,0] = re.sub(str(name), x + "_x", ts.iloc[i,0], flags=re.IGNORECASE, count=1)
-ts.to_csv(r'delexicalized\delex.csv')
+                    ts.iloc[i,1] = re.sub(str(found_name), x + "_x", ts.iloc[i,1], flags=re.IGNORECASE, count=1)
+                else:
+                    # look for the best match in the ref (e.g. for priceRange: £20-25, £20-£25 is also fine)
+                    # ?b: best match with error {e<=x}
+                    # i?: ignore case
+                    # \w+: include whole words (e.g. went wrong with match on "tart at", had to include "start at")
+                    e = len(name) - len(name.split())
+                    found_name = regex.search(r"(?b)(?i)(?:\w+" + name + "\w+){e<=" + str(e) + "}", ts.iloc[i, 1])
+                    if found_name:
+                        found_name = found_name.group(0)
+                        ts.iloc[i, 1] = re.sub(str(found_name), x + "_x", ts.iloc[i, 1], flags=re.IGNORECASE, count=1)
+                # replace the value in between brackets in the mr
+                ts.iloc[i,0] = re.sub(str(name), x + "_x", ts.iloc[i,0], flags=re.IGNORECASE, count=1)
+        except Exception:
+            print("took too long", i)
+            ts.iloc[i,1] = "XXX"
+            continue
+        # print(ts.iloc[[i]])
+
+ts.to_csv(r'delexicalized/delex3.csv')
